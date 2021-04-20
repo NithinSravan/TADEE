d426aada38a0398b85e0f5f9a263497a0f64adb2
NithinSravan <56395258+NithinSravan@users.noreply.github.com>
2021-04-20 17:06:23 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,134 @@
+function init(){
+    N = inputs[0].value;
+    if(symRad.checked){
+       d1=dInp[0].value;
+       d2=d1;
+       d3=d1;
+   }
+   else{
+       d1=dInp[0].value;
+       d2=dInp[1].value;
+       d3=dInp[2].value;
+   }
+   n=inputs[5].value; 
+   d=inputs[6].value;
+   l=inputs[7].value; 
+   R=inputs[8].value; 
+   //R=5/70
+   f=inputs[9].value; 
+   V=inputs[10].value*Math.pow(10,3);
+   Pr=inputs[11].value*Math.pow(10,6); 
+   pfr=inputs[12].value;
+
+   Dia = subDia(N,d);
+   s=stringSeparator()
+   L =inductance(N,s,d1,d2,d3)
+   Cap=capacitance(N,s,d1,d2,d3)
+   Xl = 2*Math.PI*f*l*L;
+   //Xl=25
+   Xc = 1/(2*Math.PI*f*l*Cap);
+   console.log(R,Xc,Xl)
+}
+//master function
+function master(){
+    init()
+    let str
+    let y=math.complex(0,1/Xc);
+    let z = math.complex(R*l,Xl);
+    console.log(z)
+    if(l<=80){
+        console.log("Short");
+        str="Short"
+        abcdParams("S",z)
+        console.log(A,B,C,D)
+
+    }else if(l>80&&l<=250){
+        console.log("Nominal-Pi");
+        str="Nominal-Pi"
+        abcdParams("N",z,y)
+        console.log(A,B,C,D)
+    }else if(l>250){
+        console.log("Long");
+        str="Long"
+        abcdParams("L",z,y)
+        console.log(A,B,C,D)
+    }
+    Ir= math.complex({r:receivingCurr(Pr,pfr,V),phi:-math.acos(pfr)});
+
+    let a =math.multiply(A,parseFloat(V)/math.sqrt(3))
+    let b= math.multiply(B,Ir)
+    let c=math.multiply(C,parseFloat(V)/math.sqrt(3))
+    let d =math.multiply(D,Ir)
+
+    Vs=math.add(a,b);
+    Is=math.add(c,d);
+
+    let Vr=VR(parseFloat(V)/math.sqrt(3))
+    let eff=effc(Pr,Vs,Is)
+    let comp
+    let Cs=[]
+    let Cr=[]
+    let Vp=parseFloat(V)/math.sqrt(3)
+    Cs.push((math.abs(D)*Math.pow(math.abs(Vs),2)*math.cos(math.atan(B.im/B.re)))/math.abs(B))
+    Cs.push((math.abs(D)*Math.pow(math.abs(Vs),2)*math.sin(math.atan(B.im/B.re)))/math.abs(B))
+    Cr.push((math.abs(A)*Math.pow(Vp,2)*math.cos(math.atan(B.im/B.re)))/math.abs(B))
+    Cr.push((math.abs(A)*Math.pow(Vp,2)*math.sin(math.atan(B.im/B.re)))/math.abs(B))
+    let Rc = math.abs(Vs)*Vp/math.abs(B);
+    console.log(Rc)
+    graph(Cs,Cr,Rc)
+
+
+    let inputDiv=document.getElementById("visible");
+    let inputHead=document.getElementById("inputhead");
+    inputDiv.style.display="none";
+    inputHead.style.display="none";
+    let outputDiv=document.getElementById("invisible");
+    let outputHead=document.getElementById("outputhead");
+    let graphDiv=document.getElementById("graphsec");
+    graphDiv.style.display="flex"
+    let op = document.getElementsByClassName("op");
+    outputDiv.style.display="flex"
+    outputHead.style.display="flex"
+    console.log(L)
+    Ich=chargingCurr(str,Vs);
+    op[0].innerText=str;
+    
+
+    op[1].innerText=math.round(L,5);
+    op[2].innerText=math.round(Cap,12);
+    op[3].innerText=math.round(Xl,5);
+    op[4].innerText=math.round(Xc,5);
+    op[5].innerText=math.round(A,5);
+    op[6].innerText=math.round(B,5);
+    op[7].innerText=math.round(C,5);
+    op[8].innerText=math.round(D,5);
+    op[9].innerText=math.round(Vs,5);
+    op[10].innerText=math.round(Is,5);
+    op[11].innerText=math.round(Vr,5);
+    op[12].innerText=math.round((Ps-Pr),5);
+    op[13].innerText=math.round(eff,5);
+    op[14].innerText=math.round(Ich,5);
+    if(l<=80){
+        comp=compensation()
+        op[15].innerText=math.round(comp,5)
+    }
+    else{
+        op[15].style.display="none"
+        document.getElementById('h').style.display="none"
+    }
+ 
+}
+function onCheck(){
+    if(symRad.checked){
+        dInp[1].disabled=true;
+        dInp[2].disabled=true;
+        dInp[1].value=""
+        dInp[2].value=""
+        labels[3].innerText="Enter the value of d";
+    }
+    else{
+        dInp[1].disabled=false;
+        dInp[2].disabled=false;
+        labels[3].innerText="Enter the value of d1";
+    }
+}
---
+++ util.js
@@ -0,0 +1,174 @@
+let N; //number of subconductors in bundle
+let s,d1,d2,d3; // spacings
+let n; //number of strands in a subconductor
+let d; //diameter of a strand
+let Dia; //diameter of subconductor
+let l; //length of line
+let R; //resistance of line per km
+let L; //inductance of line per km
+let Cap; //capacitance of line per km
+let Xl;//inductive reactance
+let Xc;//capcitive reactance
+let f; //power frequency
+let V; //nominal system voltage
+let Vs; // Sending end voltage
+let Is; //Sending end current
+let Ir; //Receiving end current
+let Pr; //receiving end load
+let Ps; //sending end load
+let pfr; //pf of receiving
+let r; //radius of subconductor
+let A,B,C,D; //ABCD Parameters
+let Ich; //Charging Current
+s=[]
+let comp; //compensation
+let epsilon =  8.85*Math.pow(10,-12);
+
+let sgmdIp = document.getElementById('sgmd');
+let symRad = document.getElementById('sym');
+let unsymRad = document.getElementById('unsym');
+let dInp = document.getElementsByClassName('md');
+let labels = document.querySelectorAll('span');
+let inputs = document.querySelectorAll('#params input')
+let seEnd = document.getElementById('s');
+let reEnd = document.getElementById('r');
+
+document.addEventListener("wheel", function(event){
+    if(document.activeElement.type === "number"){
+        document.activeElement.blur();
+    }
+});
+function graph(Cs,Cr,r) {
+  let graph1 = Desmos.GraphingCalculator(seEnd);
+  let graph2 = Desmos.GraphingCalculator(reEnd);
+  document.querySelectorAll('.hide')[0].style.display="block";
+  document.querySelectorAll('.hide')[1].style.display="block";
+  r/=Math.pow(10,6);
+  
+  graph1.setExpression({ id: "graph1", latex: `(x-${Cs[0]/Math.pow(10,6)})^2+(y-${Cs[1]/Math.pow(10,6)})^2=${r*r}` });
+  graph2.setExpression({ id: "graph2", latex: `(x+${Cr[0]/Math.pow(10,6)})^2+(y+${Cr[1]/Math.pow(10,6)})^2=${r*r}` });
+}
+function mgmd(d1,d2,d3){
+    return Math.cbrt(d1*d2*d3);
+}
+function sgmdRoot(prod,m){
+    return Math.pow(prod,1/(m*m))
+}
+function stringSeparator(){
+    let w = sgmdIp.value;
+    s_w = w.split(',')
+    let i=0;
+    let s=[]
+    s_w.forEach(ele => {
+     s.push(parseFloat(ele)) 
+     i++  
+    });
+    return s;
+}
+function subDia(N,d){
+    let m = (3+math.sqrt(12*N-3))/6; //number of layers
+    let D = (2*m-1)*d; //diameter of subconductor
+    return D
+}
+function receivingCurr(Pr, pfr,V){
+    let Ir=Pr/(V*pfr*math.sqrt(3));
+    return Ir;
+}
+function abcdParams(model,z,y=1){
+    let arr=[]
+    if(model === "S"){
+        A = 1
+        B =z
+        C=0
+        D=A
+    }else if(model ==="N"){
+        let a = math.add(1,math.divide(math.multiply(z,y),2))
+        A = a
+        B =z
+        let c = math.add(y,math.divide(math.multiply(z,math.multiply(y,y)),4))
+        C=c
+        D=A
+    }
+    else if(model === "L"){
+        let gamma = math.sqrt(math.multiply(z,y))
+        let Zc =math.sqrt(math.divide(z,y));
+        let a = math.cosh(gamma)
+        A = a
+        let b = math.multiply(Zc,math.sinh(gamma))
+        B =b
+        let c = math.divide(math.sinh(gamma),Zc)
+        C=c
+        D = A
+    }
+}
+
+function VR(V){
+    let Vnl= math.abs(Vs)/math.abs(A);
+    let vr=(Vnl-V)/V;
+    return vr*100;
+}
+function effc(Pr,Vs,Is){
+    Ps = 3*math.abs(Vs)*math.abs(Is)*math.cos(math.atan(Vs.im/Vs.re)-math.atan(Is.im/Is.re))
+    let eff = 100*Pr/Ps 
+    return eff;
+}
+function sgmd(Nc,sd,ele){
+    let r_dash
+    //let r=0.006
+    let r = Dia/2;
+    if(ele==="L"){
+        r_dash=r*0.7788;
+    }
+    if(ele==="C"){
+         r_dash=r;
+    }
+    let prod = 1;
+    for(let i=0;i<sd.length;i++){
+        prod*=sd[i];
+    }
+    prod=Math.pow(prod,2)
+    prod*=Math.pow(r_dash,Nc)
+    return sgmdRoot(prod,Nc);
+ }
+//inductance per km
+function inductance(N,s,d1,d2,d3){
+    let SGMD = sgmd(N,s,"L")
+    let MGMD = mgmd(d1,d2,d3)
+    let L = 2*Math.pow(10,-7)*Math.log(MGMD/SGMD);
+    console.log(L)
+    return L*1000
+}
+//capacitance per km
+function capacitance(N,s,d1,d2,d3){
+    let SGMD = sgmd(N,s,"C")
+    let MGMD = mgmd(d1,d2,d3)
+    let C = 2*Math.PI*epsilon/Math.log(MGMD/SGMD);
+    return C*1000
+}
+
+//charging current
+function chargingCurr(str,Vs){
+    if(str==="Short")
+    return 0;
+    else if(str==="Nominal-Pi"){
+        let i= math.abs(Vs)/(2*Xc)
+        console.log(Vs)
+        return i;
+    }
+    else
+    return ""
+}
+
+//Compensation
+function compensation(){
+    let Cr=[]
+    let Vp=parseFloat(V)/math.sqrt(3)
+    Cr.push((math.abs(A)*Math.pow(Vp,2)*math.cos(math.atan(B.im/B.re)))/math.abs(B))
+    Cr.push((math.abs(A)*Math.pow(Vp,2)*math.sin(math.atan(B.im/B.re)))/math.abs(B))
+    let r = Vp*Vp/math.abs(B);
+    r/=Math.pow(10,6)
+    let Qr=math.sqrt(Math.pow(r,2)-Math.pow((Pr/(3*Math.pow(10,6))+Cr[0]/Math.pow(10,6)),2)) - Cr[1]/Math.pow(10,6);
+    let Q = math.sin(math.acos(pfr))*Pr/(pfr*Math.pow(10,6))/3;
+    console.log(Q,Qr)
+    return (Q-Qr)
+}
